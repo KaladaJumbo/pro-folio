e55f7bc6a97bd389d7b73be4b1e72e8d3cd5a657
Adam <adamadolfo8@gmail.com>
2020-10-20 13:09:35 -0400

    working on js events

--- frontend-pro-folio/js/app.js
+++ frontend-pro-folio/js/app.js
@@ -1,1 +1,24 @@
 $(document).foundation();
+
+const menuUl = document.querySelector(".menu").children
+const home = menuUl[0]
+const skills = menuUl[1]
+const projects = menuUl[2]
+const contact = menuUl[3]
+
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    addEventListenersToButtons();
+})
+
+function addEventListenersToButtons() {
+    console.log("")
+    home.addEventListener("click", () => {});
+    skills.addEventListener("click", () => {});
+    projects.addEventListener("click", () => {});
+    contact.addEventListener("click", () => {
+        console.log("test")
+    });
+}
