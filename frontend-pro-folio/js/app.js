6732befede4c5331a504b1b782171d53729e472b
Adam <adamadolfo8@gmail.com>
2020-10-20 17:35:50 -0400

    working on post to server

--- frontend-pro-folio/js/app.js
+++ frontend-pro-folio/js/app.js
@@ -1,10 +1,19 @@
 $(document).foundation();
 
+//navbar
 const menuUl = document.querySelector(".menu").children
 const home = menuUl[0]
 const skills = menuUl[1]
 const projects = menuUl[2]
 const contact = menuUl[3]
+//minibar
+const minibar = document.querySelector(".top-list-right").children
+const clientLogin = minibar[0]
+const signup = minibar[1]
+const support = minibar[2]
+//login
+const form = document.querySelector("#signin")
+
 
 
 
@@ -14,11 +23,100 @@ document.addEventListener("DOMContentLoaded", () => {
 })
 
 function addEventListenersToButtons() {
-    console.log("")
+    //navbar
     home.addEventListener("click", () => {});
     skills.addEventListener("click", () => {});
     projects.addEventListener("click", () => {});
-    contact.addEventListener("click", () => {
-        console.log("test")
+    contact.addEventListener("click", () => {});
+    //minibar
+    clientLogin.addEventListener("click", () => {
+        login()
     });
+    signup.addEventListener("click", () => {
+        signUp()
+    });
+    support.addEventListener("click", () => {});
+}
+
+function signUp() {
+    form.innerHTML = ""
+    form.innerHTML = `
+    <form class="sign-up">
+     <div class="sign-in-form">
+       <h4 class="text-center">Sign-Up</h4>
+
+       <label for="sign-in-form-username">First Name</label>
+       <input type="text" class="sign-in-form-username" id="sign-in-form-firstname">
+
+       <label for="sign-in-form-username">Last Name</label>
+       <input type="text" class="sign-in-form-username" id="sign-in-form-lastname">
+
+       <label for="sign-in-form-username">Username</label>
+       <input type="text" class="sign-in-form-username" id="sign-in-form-username">
+
+       <label for="sign-in-form-password">Password</label>
+       <input type="text" class="sign-in-form-password" id="sign-in-form-password">
+       <button type="submit" class="sign-in-form-button">Create Account</button>
+     </div>
+   </form>
+    `
+    let signUpForm = document.querySelector(".sign-up")
+    signUpForm.addEventListener("submit", (e) => {
+        e.preventDefault();
+        handleSignUp(e)
+    })
+}
+
+function login() {
+    form.innerHTML = ""
+    form.innerHTML = `
+    <form class="login">
+     <div class="sign-in-form">
+       <h4 class="text-center">login</h4>
+       <label for="sign-in-form-username">Username</label>
+       <input type="text" class="sign-in-form-username" id="sign-in-form-username">
+       <label for="sign-in-form-password">Password</label>
+       <input type="text" class="sign-in-form-password" id="sign-in-form-password">
+       <button type="submit" class="sign-in-form-button">Sign In</button>
+     </div>
+   </form>
+    `
+    let loginForm = document.querySelector(".login")
+    loginForm.addEventListener("submit", (e) => {
+        e.preventDefault();
+        handleLogin(e)
+    })
+}
+
+function handleSignUp(e) {
+    let fName = e.target[0].value
+    let lName = e.target[1].value
+    let uName = e.target[2].value
+    let password = e.target[3].value
+
+    let userObj = {
+        first_name: fName,
+        last_name: lName,
+        username: uName,
+        password: password
+    }
+
+    let meta = {
+        method: "POST",
+        headers: {"Content-Type":"application/json"}, 
+        body: JSON.stringify(userObj)
+    }
+
+    fetch("http//localhost3000/users", meta)
+    .then(r => r.json())
+    .then(data => {
+        console.log(data)
+    })
+    .catch(error => {
+        console.log("failure")
+    })
+}
+
+function handleLogin(e) {
+
 }
