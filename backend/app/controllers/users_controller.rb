1ec4e22d6dea95cfd86f3eee70d83da0a269bb7d
KaladaJumbo <wkalada1@gmail.com>
2020-10-21 03:02:51 -0400

    show page controller not tested

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -7,21 +7,26 @@ class UsersController < ApplicationController
     end
 
     def show
-        user = User.find_by(username: params[:id])
+        user = User.find_by(username: params[:username])
+        if user && user.authenticate(params[:password])
+            render json: user
+        # else  
+        #     render :json => { 
+        #         :errors => user.errors.full_messages }, :status => 422
+        end
 
-        render json: user
     end
 
     def create
         user = User.new(first_name: params[:first_name], last_name: params[:last_name], username: params[:username], password: params[:password]) 
         if user.save
             render json: user
-        # else 
-        #     render json: {status: "error", code: 3000, message: user.errors.full_message}
+        else 
+            render :json => { 
+            :errors => user.errors.full_messages 
+            }, :status => 422
         end
     end
 
-    def login
-        #local session js and send login for authentication... something something ... 
-    end
+
 end
